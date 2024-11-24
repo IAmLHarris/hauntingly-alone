2670c9613bfdd9861619974af21015a882f52079
IAmLHarris <har21091@byui.edu>
2024-11-24 14:27:36 -0600

    Week 2 finally works

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,8 @@ const routes = require("express").Router();
 const Post = require("../data/post");
 
 routes.get("/", (req, res) => {
-  res.send("Database Week 2");
+  res.send("Database");
 });
-
-// routes.post("/", (req, res) => {
-//   console.log(req.body, "index thing??");
-// });
 
 routes.use("/users", require("./users"));
 
